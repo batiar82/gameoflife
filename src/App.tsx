09f85499efab297add62f6c3ccac49cb6844e733
mariano <123>
2020-09-19 12:21:10 -0300

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, FC, useCallback, useRef } from 'react';
 import './App.css';
+import produce from "immer"
+const numRows = 50;
+const numCols = 50;
+const operations = [
+  [0, 1],
+  [1, 1],
+  [-1, 1],
+  [-1, 0],
+  [1, 0],
+  [-1, -1],
+  [0, -1],
+  [1, -1]
+]
+const getNeighbors = (grid: boolean[][], i: number, j: number) => {
+  let neighbors = 0;
+  operations.forEach(op => {
+    const k = i + op[0];
+    const l = j + op[1];
+    if (k >= 0 && l >= 0 && k < 50 && l < 50 && grid[k][l])
+      neighbors++;
+  })
+  return neighbors;
+}
 
-function App() {
+const App: FC = () => {
+  const [grid, setGrid] = useState(() => {
+    let grid = [];
+    for (let i = 0; i < numRows; i++) {
+      grid.push(Array.from(Array(numCols), () => false))
+    }
+    return grid;
+  })
+  const [running, setRunning] = useState(false);
+  const runningRef = useRef(false);
+  runningRef.current = running;
+
+  const randomize = () => {
+    let grid = [];
+    for (let i = 0; i < numRows; i++) {
+      grid.push(Array.from(Array(numCols), () => Math.random() > 0.7))
+    }
+    setGrid(grid);
+  }
+  
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) return
+    setGrid(g => {
+      return produce(g, drafGrid => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numRows; j++) {
+            const neighbors = getNeighbors(g, i, j);
+            if (neighbors > 0) { console.log(neighbors) }
+            if (!g[i][j] && neighbors === 3) {
+              drafGrid[i][j] = true;
+            }
+            else if (g[i][j] && (neighbors < 2 || neighbors > 3)) {
+              drafGrid[i][j] = false;
+            }
+          }
+        }
+      });
+      // Simulate
+    })
+    setTimeout(() => runSimulation(), 200);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={() => {
+        setRunning(!running)
+        setTimeout(
+          runSimulation, 1000);
+      }}>{running ? 'Stop' : 'Start'}</button>
+      <button onClick={randomize} disabled={running}>Randoimze</button>
+      <div style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(50,20px)',
+        gridTemplateRows: 'repeat(50,20px)'
+      }
+
+      }>
+        {grid.map((row, i) => row.map((col, j) => <div key={`${i}-${j}`} style={{
+          width: 20,
+          height: 20,
+          border: '1px solid black',
+          background: grid[i][j] ? 'pink' : undefined
+        }}
+          onClick={() => {
+            const newGrid = produce(grid, draftGrid => {
+              draftGrid[i][j] = !grid[i][j];
+            })
+            setGrid(newGrid);
+          }}
+        />
+        ))}
+
+      </div>
+    </>
   );
 }
 
