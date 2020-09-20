66c9f2433d4d6fbe717225278a4ca2f51b8d7a23
mariano <123>
2020-09-20 19:07:53 -0300

    GameOfLike custom hook & cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React, { useState, FC, useCallback, useRef, useEffect } from "react";
+import React, {FC } from "react";
 import "./App.css";
-import produce from "immer";
 import Button from "react-bootstrap/Button";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Container from "react-bootstrap/Container";
@@ -8,81 +7,18 @@ import Jumbotron from "react-bootstrap/Jumbotron";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import RangeSlider from "react-bootstrap-range-slider";
-import Form from "react-bootstrap/Form";
-const numRows = 50;
-const numCols = 50;
-const initialRandomWeight = 100;
-const operations = [
-  [0, 1],
-  [1, 1],
-  [-1, 1],
-  [-1, 0],
-  [1, 0],
-  [-1, -1],
-  [0, -1],
-  [1, -1],
-];
-const getNeighbors = (grid: boolean[][], i: number, j: number) => {
-  let neighbors = 0;
-  operations.forEach((op) => {
-    const k = i + op[0];
-    const l = j + op[1];
-    if (k >= 0 && l >= 0 && k < 50 && l < 50 && grid[k][l]) neighbors++;
-  });
-  return neighbors;
-};
-
-const getNewGrid = () => {
-  let grid: boolean[][] = [];
-  for (let i = 0; i < numRows; i++) {
-    grid.push(Array.from(Array(numCols), () => false));
-  }
-  return grid;
-};
+import { useGameOfLife } from "./GameOfLife";
 
 const App: FC = () => {
-  const [grid, setGrid] = useState(getNewGrid);
-
-  const [running, setRunning] = useState(false);
-  const [randomWeight, setRandomWeight] = useState(initialRandomWeight);
-  const runningRef = useRef(false);
-  useEffect(() => {
-    randomize();
-  }, [randomWeight]);
-  runningRef.current = running;
-
-  const reset = () => {
-    setGrid(getNewGrid());
-  };
-  const randomize = () => {
-    let grid: boolean[][] = [];
-    for (let i = 0; i < numRows; i++) {
-      grid.push(
-        Array.from(Array(numCols), () => Math.random() > randomWeight / 100)
-      );
-    }
-    setGrid(grid);
-  };
-
-  const runSimulation = useCallback(() => {
-    if (!runningRef.current) return;
-    setGrid((g) => {
-      return produce(g, (drafGrid) => {
-        for (let i = 0; i < numRows; i++) {
-          for (let j = 0; j < numRows; j++) {
-            const neighbors = getNeighbors(g, i, j);
-            if (!g[i][j] && neighbors === 3) {
-              drafGrid[i][j] = true;
-            } else if (g[i][j] && (neighbors < 2 || neighbors > 3)) {
-              drafGrid[i][j] = false;
-            }
-          }
-        }
-      });
-      // Simulate
-    });
-    setTimeout(() => runSimulation(), 200);
-  }, []);
+  const {
+    grid,
+    toggleRunning,
+    running,
+    handleCellClick,
+    reset,
+    randomWeight,
+    setRandomWeight
+    } = useGameOfLife({numRows: 50})
 
   return (
     <Jumbotron>
@@ -97,10 +33,7 @@ const App: FC = () => {
             >
               <Button
                 variant="outline-primary"
-                onClick={() => {
-                  setRunning(!running);
-                  setTimeout(runSimulation, 1000);
-                }}
+                onClick={toggleRunning}
               >
                 {running ? "Stop" : "Start"}
               </Button>
@@ -115,15 +48,14 @@ const App: FC = () => {
           </Col>
         </Row>
         <Row className="justify-content-xs-center">
-          <Col xs={12} style={{ display: "flex", justifyContent: "center"}}>
-            <span style={{marginRight: "20px"}}>Randomize</span>
-          <RangeSlider
+          <Col xs={12} style={{ display: "flex", justifyContent: "center" }}>
+            <span style={{ marginRight: "20px" }}>Randomize</span>
+            <RangeSlider
               value={randomWeight}
               tooltip="off"
               onChange={(e) => setRandomWeight(e.target.value)}
               disabled={running}
-            />   
-            
+            />
           </Col>
         </Row>
         <Row>
@@ -131,7 +63,7 @@ const App: FC = () => {
             <div
               style={{
                 display: "grid",
-                width: numRows * 20,
+                width: 50 * 20,
                 gridTemplateColumns: "repeat(50,20px)",
                 gridTemplateRows: "repeat(50,20px)",
                 borderTop: "1px solid black",
@@ -147,12 +79,7 @@ const App: FC = () => {
                       borderRight: "1px solid black",
                       background: grid[i][j] ? "pink" : undefined,
                     }}
-                    onClick={() => {
-                      const newGrid = produce(grid, (draftGrid) => {
-                        draftGrid[i][j] = !grid[i][j];
-                      });
-                      setGrid(newGrid);
-                    }}
+                    onClick={() => handleCellClick(i,j)}
                   />
                 ))
               )}
---
+++ src/GameOfLife.tsx
@@ -0,0 +1,99 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+import produce from "immer";
+
+const numRows = 50;
+const numCols = 50;
+const initialRandomWeight = 100;
+const operations = [
+  [0, 1],
+  [1, 1],
+  [-1, 1],
+  [-1, 0],
+  [1, 0],
+  [-1, -1],
+  [0, -1],
+  [1, -1],
+];
+const getNeighbors = (grid: boolean[][], i: number, j: number) => {
+  let neighbors = 0;
+  operations.forEach((op) => {
+    const k = i + op[0];
+    const l = j + op[1];
+    if (k >= 0 && l >= 0 && k < 50 && l < 50 && grid[k][l]) neighbors++;
+  });
+  return neighbors;
+};
+
+const getNewGrid = (cellResolverFn = ()=> false) => {
+  let grid: boolean[][] = [];
+  for (let i = 0; i < numRows; i++) {
+    grid.push(Array.from(Array(numCols), cellResolverFn));
+  }
+  return grid;
+};
+
+const randomize = (weight) => {
+//   let grid: boolean[][] = [];
+//   for (let i = 0; i < numRows; i++) {
+//     grid.push(Array.from(Array(numCols), () => Math.random() > weight / 100));
+//   }
+//   return grid;
+return getNewGrid(()=> Math.random() > weight / 100);
+};
+
+export function useGameOfLife({ numRows }) {
+  const [grid, setGrid] = useState(getNewGrid);
+  const [running, setRunning] = useState(false);
+  const runningRef = useRef(false);
+  runningRef.current = running;
+  const [randomWeight, setRandomWeight] = useState(initialRandomWeight);
+
+  useEffect(() => {
+    setGrid(randomize(randomWeight));
+  }, [randomWeight]);
+
+  const reset = () => {
+    setRandomWeight(100);
+    setGrid(getNewGrid());
+  };
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) return;
+    setGrid((g) => {
+      return produce(g, (drafGrid) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numRows; j++) {
+            const neighbors = getNeighbors(g, i, j);
+            if (!g[i][j] && neighbors === 3) {
+              drafGrid[i][j] = true;
+            } else if (g[i][j] && (neighbors < 2 || neighbors > 3)) {
+              drafGrid[i][j] = false;
+            }
+          }
+        }
+      });
+      // Simulate
+    });
+    setTimeout(() => runSimulation(), 200);
+  }, [numRows]);
+  const handleCellClick = (row, col) => {
+    const newGrid = produce(grid, (draftGrid) => {
+      draftGrid[row][col] = !grid[row][col];
+    });
+    setGrid(newGrid);
+  };
+  const toggleRunning = () => {
+    setRunning(!running);
+    setTimeout(runSimulation, 1000);
+  };
+
+  return {
+    grid,
+    handleCellClick,
+    toggleRunning,
+    running,
+    randomWeight,
+    setRandomWeight,
+    reset,
+  };
+}
